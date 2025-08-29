bd0c5a54baa6d5fc80fbc9c2bbddada17d20b4ef
Victor Muhita <wangombevictor5@gmail.com>
2025-08-29 10:30:11 +0300

    style native app

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -2,13 +2,10 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.largeText}>
-        Typescript is great if you practice more
-      </Text>
-      <Text style={styles.mediumText}>
-        React Native provides you a single codebase for cross platforms
-      </Text>
+    <View>
+      <Text>Entry Screen - Awesome</Text>
+      <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+      <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
       <Text style={styles.smallText}>ALX is awesome</Text>
     </View>
   );
